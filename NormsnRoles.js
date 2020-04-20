c4d443943ef6e589a247962f6222e2dae93dd1d6
johnoh26 <john.oh26@gmail.com>
2020-04-20 14:56:06 -0400

    norms and team contract

---
+++ NormsnRoles.js
@@ -0,0 +1,49 @@
+// # Roles (rotation)
+
+// Taskmaster
+// -Decide what feature(s) to focus on (big ticket item)
+// -Teammates decide what they want to work on
+// -Make appropriate tickets
+// -Organize tickets as you progress
+
+// Gitmaster
+// -Determine which pull request to merge with the mater branch
+// -Ensure merging EOD everyday
+
+// Testmaster
+// -Write own tests
+// -Make sure it can fail
+// -Review other people’s tests
+
+// # Norms
+
+// Update the team on progress BOD and EOD
+
+// Morning Pull Party (BOD when we all meet up)
+
+// Commit and push all updates by 5pm everyday
+
+// Stay on Zoom except for Lunch
+
+// Vote on disagreements.. Flip a coin if needed
+
+// Overcommunicate when frustrated
+
+// One feature per branch
+
+// Create new files for each new feature
+
+// Help ticket process?
+
+// 1. Google, Stack Overflow, Youtube, Past Workshops (30 minutes max)
+// 2. Try pair programming (15 minutes)
+// 3. Ask all the teammates (15 minutes)
+// 4. Submit a help ticket (Ziv, Jess)
+//    (while waiting for instructors to respond, work on another feature)
+
+// Make tickets for everything
+// Type: specific feature (name)
+// (eg) Debug: login with “Jess” does not work (Alex)
+
+// Work Schedule?
+// 9:30am - 6pm + alpha (voluntary)
