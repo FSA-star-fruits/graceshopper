ea27c50781aa429d5ba43e0797598ed7b917b706
Alexander Zhao <azhao.invest@gmail.com>
2020-04-20 22:24:56 -0400

    Feature to show all Cars added

--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const db = require('../server/db')
-const {User} = require('../server/db/models')
+const {User, Car} = require('../server/db/models')
 
 async function seed() {
   await db.sync({force: true})
@@ -9,7 +9,35 @@ async function seed() {
 
   const users = await Promise.all([
     User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
+    User.create({email: 'murphy@email.com', password: '123'}),
+    // 'Toyota', 'Sequoia', 'http://dummyimage.com/223x165.bmp/dddddd/000000', 85156, 2012, 'Indigo', true
+    Car.create({
+      brand: 'Toyota',
+      name: 'Sequoia',
+      imageUrl: 'http://dummyimage.com/223x165.bmp/dddddd/000000',
+      price: 85156,
+      year: 2012,
+      color: 'Indigo',
+      isNew: true
+    }),
+    Car.create({
+      brand: 'Toyota',
+      name: 'Sequoia',
+      imageUrl: 'http://dummyimage.com/223x165.bmp/dddddd/000000',
+      price: 85156,
+      year: 2013,
+      color: 'Indigo',
+      isNew: true
+    }),
+    Car.create({
+      brand: 'Toyota',
+      name: 'Sequoia',
+      imageUrl: 'http://dummyimage.com/223x165.bmp/dddddd/000000',
+      price: 85156,
+      year: 2014,
+      color: 'Indigo',
+      isNew: true
+    })
   ])
 
   console.log(`seeded ${users.length} users`)
