4fc08557de32930170092c056e2f76a175fe0823
azhao8922 <60079013+azhao8922@users.noreply.github.com>
2020-04-22 16:44:17 -0400

    Create indexCopyForAssoc.js
    a copy made so i can work on this branch, also a test to see if this method makes sense to collaborate on the same branch

---
+++ server/db/models/indexCopyForAssoc.js
@@ -0,0 +1,35 @@
+const db = require('../db')
+const User = require('./user')
+const Order = require('./order')
+const CartItem = require('./cartItem')
+const Car = require('./car')
+
+/**
+ * If we had any associations to make, this would be a great place to put them!
+ * ex. if we had another model called BlogPost, we might say:
+ *
+ *    BlogPost.belongsTo(User)
+ */
+
+Order.belongsTo(User)
+User.hasOne(Order)
+
+Order.hasOne(CartItem)
+CartItem.belongsTo(Order)
+
+Car.belongsTo(CartItem)
+CartItem.hasOne(Car)
+
+/**
+ * We'll export all of our models here, so that any time a module needs a model,
+ * we can just require it from 'db/models'
+ * for example, we can say: const {User} = require('../db/models')
+ * instead of: const User = require('../db/models/user')
+ */
+module.exports = {
+  User,
+  Order,
+  CartItem,
+  db,
+  Car
+}
