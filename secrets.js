49d72206dcbd859c4712faae8c1a0d21aa4fbb0d
Cesar Nina <c3sar207@gmail.com>
2020-04-23 00:44:22 -0400

    added a faker component

--- secrets.js
+++ secrets.js
@@ -1,14 +1,22 @@
-// Copied from the lecture to check if user is admin
-const isAdminMiddleware = (req, res, next) => {
-  const currentUser = req.session.currentUser
-  if (currentUser && currentUser.isAdmin) {
-    next()
-  } else {
-    const error = new Error('Only for Admin')
-    error.status = 401
-    next(error)
-  }
+const secrets = {
+  clientID:
+    '822653004430-bbliravqc9a0qolf315n6n1vjkribqo0.apps.googleusercontent.com',
+  clientSecret: 'V1fJg0CWhOF7UElsUYoSSGGr',
+  callbackURL: 'http://127.0.0.1:8080/auth/johnoh/verify'
 }
+module.exports = secrets
+
+// Copied from the lecture to check if user is admin
+// const isAdminMiddleware = (req, res, next) => {
+//   const currentUser = req.session.currentUser
+//   if (currentUser && currentUser.isAdmin) {
+//     next()
+//   } else {
+//     const error = new Error('Only for Admin')
+//     error.status = 401
+//     next(error)
+//   }
+// }
 
 // For User model add a column for admin
 // const User = db.define({
